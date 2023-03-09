ae26e4098bb873c16708a86b5495365654074c54
Piti Ongmongkolkul <piti118@gmail.com>
2023-03-09 13:25:54 +0700

    update

--- bird.py
+++ bird.py
@@ -1,23 +1,44 @@
 import time
 import flappy_bird_gym
+import numpy
+from dataclasses import dataclass
+import numpy as np
 env = flappy_bird_gym.make("FlappyBird-v0")
 
-obs = env.reset()
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space.sample()  # for a random action
+@dataclass
+class Bird:
+    w: np.ndarray
 
-    # Processing:
-    obs, reward, done, info = env.step(action)
+    def should_i_jump(self, obs) -> int:
+        z = self.w[0] + self.w[1]*obs[0] + self.w[2]*obs[1]
+        return 1 if z > 0 else 0
+# karan = Bird([5, 8, 2])
 
-    # Rendering the game:
-    # (remove this two lines during training)
-    env.render()
-    time.sleep(1 / 30)  # FPS
+def play_one_game(bird: Bird) -> int:
+    obs = env.reset()
+    score = 0
+    while True:
+        score+=1
+        # Next action:
+        # (feed the observation to your agent here)
+        action = bird.should_i_jump(obs)  # for a random action
 
-    # Checking if the player is still alive
-    if done:
-        break
+        # Processing:
+        obs, reward, done, info = env.step(action)
+        print(obs, action)
 
-env.close()
+        # Rendering the game:
+        # (remove this two lines during training)
+        #env.render()
+        #time.sleep(1 / 60)  # FPS
+
+        # Checking if the player is still alive
+        if done:
+            break
+
+    env.close()
+    return score
+
+humming = Bird([ -22.05717347,   21.23837877, -552.75222079])
+score = play_one_game(humming)
+print(humming, score)
